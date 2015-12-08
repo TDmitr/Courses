61114b74e95139733bd5ccf3377b8c0c71a88fb7
TDmitr <tanyamechanical@gmail.com>
2015-12-08 21:04:17 +0200

    Tasks

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            int num = int.Parse(user);
+            for (int i = 1; i < num+1; i++)
+            {
+                if (num%i == 0)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("input.txt"); 
+            string str = ""; 
+
+            while (!streamReader.EndOfStream) 
+            {
+                str += streamReader.ReadLine(); 
+            }
+
+            string[] newstr = str.Split('1');
+
+            StreamWriter write_text;
+            FileInfo file = new FileInfo("output.txt");
+            write_text = file.AppendText(); 
+            write_text.WriteLine(newstr.Length-1); 
+            write_text.Close(); 
+        }
+    }
+}
